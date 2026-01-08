27c9f84b7084231f64f0013676fa13e43d932a09
Jared <jkarpiak@nait.ca>
2026-01-08 11:02:44 -0700

    Add project files.

---
+++ HelloA02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloA02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, A02! You guys seem awesome!");
+
+        }
+    }
+}
